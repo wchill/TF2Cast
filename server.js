42452327281ba76f622ccb9d32a9f7fcb49646e3
Paul Gibbons <paultgibbons@gmail.com>
2014-12-04 00:44:52 -0600

    Add backbone of described documentation. Include a test js file that calls one of these functions     that has info sent to the clients

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-var express = require('express');
+var express = require('express')
+  , bodyParser      = require('body-parser')
+  , json            = require('json')
+  , urlencode       = require('urlencode');
 var app = express();
 var server = require('http').Server(app);
 server.listen(8000);
 var io = require('socket.io')(server);
+app.use(bodyParser());
+
 
 var _messages = [
   {text: 'Hi', id: 0}
 ];
+
+// should eventually be removed
+var _errors = [];
 
 function createMessage(text) {
   var newMessage = {text: text, id: _messages.length};
@@ -27,4 +35,38 @@ io.on('connection', function(socket) {
   socket.on('test_client', function(str) {
     io.emit('message_from_server', createMessage(str));
   });
+});
+
+app.post('/api/private/bootstrap', function(req, res) {
+  res.json(_errors);
+});
+
+app.post('/api/private/death', function(req, res) {
+  console.log(req.body);
+  io.emit('message_from_server', createMessage(req.body.killer + ' has killed ' + req.body.killed))
+  res.json(_errors);
+});
+
+app.post('/api/private/respawn', function(req, res) {
+  res.json(_errors);
+});
+
+app.post('/api/private/connected', function(req, res) {
+  res.json(_errors);
+});
+
+app.post('/api/private/disconnected', function(req, res) {
+  res.json(_errors);
+});
+
+app.post('/api/private/teamswitch', function(req, res) {
+  res.json(_errors);
+});
+
+app.post('/api/private/playerscores', function(req, res) {
+  res.json(_errors);
+});
+
+app.post('/api/private/roundover', function(req, res) {
+  res.json(_errors);
 });
