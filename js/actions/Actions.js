8dcfe6f239f2072f41b1441f893f49a4efb30ff6
Josh Halstead <jhalstead85@gmail.com>
2014-12-17 22:24:51 -0600

    TeamStore implemented based on raw API
    The interfaces.md API has some inconsistency and duplication
    that could be refined in the future. However, until then the
    TeamStore logic is being implemented based of the existing raw
    API.
    
    Also reverted some earlier naming changes in Actions.js that were
    inappropriate.

--- js/actions/Actions.js
+++ js/actions/Actions.js
@@ -8,40 +8,100 @@ var Actions = {
     });
   },
 
-  playerDeath: function(payload) {
+  playerDeath: function(data) {
     AppDispatcher.handleServerAction({
       type: Constants.DEATH,
-      payload: payload
+      message: data
     });
   },
 
-  playerConnect: function(payload) {
+  playerConnect: function(data) {
     AppDispatcher.handleServerAction({
       type: Constants.CONNECT,
-      payload: payload
+      message: data
     });
   },
 
-  playerDisconnect: function() {
+  playerDisconnect: function(data) {
     AppDispatcher.handleServerAction({
       type: Constants.DISCONNECT,
-      payload: payload
+      message: data
     });
   },
 
-  playerUpdate: function() {
+  playerUpdate: function(data) {
     AppDispatcher.handleServerAction({
       type: Constants.PLAYER_UPDATE,
-      payload: payload
+      message: data
     });
   },
 
-  teamUpdate: function() {
+  teamUpdate: function(data) {
     AppDispatcher.handleServerAction({
       type: Constants.TEAM_UPDATE,
-      payload: payload
+      message: data
     });
   }
+
+
+  /************************************************* */
+
+  bootstrap: function(data) {
+    AppDispatcher.handleServerAction({
+      type: Constants.BOOTSTRAP,
+      message: data
+    });
+  },
+
+  death: function(data) {
+    AppDispatcher.handleServerAction({
+      type: Constants.DEATH,
+      message: data
+    });
+  },
+
+  respawn: function(data) {
+    AppDispatcher.handleServerAction({
+      type: Constants.RESPAWN,
+      message: data
+    });
+  },
+
+  connected: function(data) {
+    AppDispatcher.handleServerAction({
+      type: Constants.CONNECTED,
+      message: data
+    });
+  },
+
+  disconnected: function(data) {
+    AppDispatcher.handleServerAction({
+      type: Constants.DISCONNECTED,
+      message: data
+    });
+  },
+
+  teamswitch: function(data) {
+    AppDispatcher.handleServerAction({
+      type: Constants.TEAM_SWITCH,
+      message: data
+    });
+  },
+
+  playerscores: function(data) {
+    AppDispatcher.handleServerAction({
+      type: Constants.PLAYERS_SCORES,
+      message: data
+    });
+  },
+
+  roundover: function(data) {
+    AppDispatcher.handleServerAction({
+      type: Constants.ROUND_OVER,
+      message: data
+    });
+  }
+
 };
 
 module.exports = Actions;
--- js/stores/TeamStore.js
+++ js/stores/TeamStore.js
@@ -7,115 +7,107 @@ var socketHandler = require('../socketHandler');
 
 // Team: {
 //   id: int
-//   players: Array<player>
+//   players: map <playerid, player>
 //   score: int
-// }
-var teams = [];
-
+// }s
 // Player: {
 //   id: string
 //   team: int // ???
 //   name: string
 //   score: int
-//   etc.
+//   avatar: string // URL
+//   dead: boolean
 // }
+var _RED = 0;
+var _BLU = 1;
+var _SPEC = 2;
 
-var RED_ID = 0;
-var RED = {id: RED_ID, players: [], score: 0};
-
-var BLU_ID = 1;
-var BLU = {id: BLU_ID, players: [], score: 0};
-
-var SPEC_ID = 2;
-var SPECTATORS = {id: SPEC_ID, players: [], score: 0};
+var _players = {};
+var _teamScores = [0, 0, 0]; // [RED, BLU, SPEC]
 
 function reset() {
-  teams = [RED, BLU];
+  _players = {};
+  _teamScores = [0, 0, 0];
 }
 
-function isValidTeam(teamId) {
-  return RED_ID <= teamId && teamId <= SPEC_ID;
+function isBot(playerid) {
+  return playerid[0] !== '[';
 }
 
-function addTeam(teamId, team) {
-  if (isValidTeam(teamId)) {
-    teams[teamId] = team;
+function playersToArray() {
+  var a = [];
+
+  for (p in _players) {
+    if (_players.hasOwnProperty(p)) {
+      a.push(_players[p]);
+    }
+  }
+
+  return a;
+}
+
+function getTeam(teamid) {
+  var a = playersToArray();
+  var teamPlayers = a.filter( function (p) {return p.team === teamid;} );
+
+  return {
+    id: teamid,
+    players: teamPlayers,
+    score: teamScores[teamid]
   }
 }
 
-function fetchTeam(teamId) {
-  if (isValidTeam(teamId)) {
-    return teams[teamId];
-  }
-
-  return null;
+function getRedTeam() {
+  return getTeam(_RED);
 }
 
-function addPlayer(teamId, player) {
-  var existingPlayer = fetchPlayer(player.id);
-  if (existingPlayer !== null) {
-    updatePlayer()
-  }
-
-  teams[teamId].push(player);
-  teams[teamId].sort
+function getBluTeam() {
+  return getTeam(_BLU);
 }
 
-// TODO Find a better way?
-function removePlayer(playerId) {
-  teams.forEach(function(team) {
-    team.players = team.players.filter(function(player) {
-      if (player.id !== playerId) {
-        return player;
-      }
+function getSpectators() {
+  return getTeam(_SPEC);
+}
+
+function getAllTeams() {
+  return [getRedTeam(), getBluTeam(), getSpectators()];
+}
+
+function bootstrapPlayers(players, teamid) {
+  _players[player.playerid] = {
+    id: player.playerid,
+    name: isBot(player) ? player.playerid : '',
+    avatar: '',
+    score: player.score,
+    team: teamid,
+    alive: true
+  };
+
+  // Not sure if we'll need to through this in a closure or not...
+  if (!isBot(player)) {
+
+    // TODO extract into function call ???
+    var apiKey = '8FB18602A84F393E886D7E47F8FCF2D1';
+    var url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=';
+    url += apiKey + '&steamids=' + player.playerid;
+
+    var userRequest = xhr('GET', url);
+    userRequest.success(function(data) {
+      _players[player.playerid].name = data.playername;
+      _players[player.playerid].avatar = data.avatar; // url to 32 x 32px
     });
-  });
-}
+    userRequest.error(function(data) {
+      _players[player.playerid].name = player.playerid;
+      // TODO Add default avatar in local assets folder
+    });
 
-function updatePlayer(playerId, playerDelta) {
-  var player = fetchPlayer(playerId);
-  if (player === null) {
-    return;
   }
-
-  for (var key in playerDelta) {
-    if (playerDelta.hasOwnProperty(key) &&
-      player.hasOwnProperty(key)) {
-        player[key] = playerDelta[key];
-      }
-    }
-  }
-
-function fetchPlayer(playerId) {
-  teams.forEach(function(team) {
-    var player = fetchPlayerFromTeam(team.id, playerId);
-    if (player !== null) {
-      return player;
-    }
-  });
-
-  return null;
-}
-
-function fetchPlayerFromTeam(teamId, playerId) {
-  var team = fetchTeam(teamId);
-  if (team === null) {
-    return null;
-  }
-
-  team.players.forEach(function(player) {
-    if (player.id === playerId) {
-      return player;
-    }
-  });
-
-  return null;
 }
 
 var TeamStore = assign({}, EventEmitter.prototype, {
 
   getTeams: function() {
-    return teams;
+    return getAllTeams();
   },
 
   emitChange: function() {
@@ -133,20 +125,132 @@ var TeamStore = assign({}, EventEmitter.prototype, {
 
 AppDispatcher.register(function(payload) {
   var action = payload.action;
+  var type = action.type;
+  var message = action.message; // Message from plugin
 
-  switch(action.type) {
-    case Constants.DEATH:
-      addMessage(action.payload);
-      MessageStore.emitChange();
-      break;
+  console.log(action);
 
+  switch(type) {
     case Constants.RESET:
+      console.log('RESET');
       reset();
-      MessageStore.emitChange();
+      TeamStore.emitChange();
       break;
-  }
 
-  return true;
+    case Constants.BOOTSTRAP:
+      console.log('BOOTSTRAP');
+      reset(); // Bootstrap should only be called at the start of a game
+
+      if (message.hasOwnPropert('red_wins')
+        && message.hasOwnProperty('blu_wins')
+        && message.hasOwnProperty('red_players')
+        && message.hasOwnProperty('blu_players')) {
+
+        _teamScores[_RED] = message.red_wins;
+        _teamScores[_BLU] = message.blu_wins;
+
+        bootstrapPlayers(message.red_players, _RED);
+        bootstrapPlayers(message.blu_players, _BLU);
+        bootstrapPlayers(message.spectators, _SPEC);
+
+        TeamStore.emitChange();
+      } else {
+        console.log("Malformed bootstrap event");
+      }
+      break;
+
+    case Constants.DEATH:
+      console.log('DEATH');
+      if (message.hasOwnProperty('victim')) {
+        _players[message.victim].alive = false;
+        TeamStore.emitChange();
+      } else {
+        console.log("Malformed death event");
+      }
+      break;
+
+    case Constants.RESPAWN:
+      console.log('RESPAWN');
+      if (message.hasOwnProperty('player')) {
+        _players[message.player].alive = true;
+        TeamStore.emitChange();
+      } else {
+        console.log("Malformed respawn event");
+      }
+      break;
+
+    case Constants.CONNECTED:
+      console.log('CONNECTED');
+      if (message.hasOwnProperty('player') && message.hasOwnProperty('team')) {
+        bootstrapPlayers([message.player], message.team);
+        TeamStore.emitChange();
+      } else {
+        console.log("Malformed connected event");
+      }
+      break;
+
+    case Constants.DISCONNECTED:
+      console.log('DISCONNECTED');
+
+      // TODO Is this desired?
+      if (message.hasOwnPropery('player')) {
+        delete _players[message.player];
+        TeamStore.emitChange();
+      } else {
+        console.log("Malformed disconnected event");
+      }
+      break;
+
+    case Constants.TEAM_SWITCH:
+      console.log('TEAM SWITCH');
+
+      if (message.hasOwnProperty('player') && message.hasOwnProperty('team')) {
+        _players[message.player].team = message.team;
+        TeamStore.emitChange();
+      } else {
+        console.log("Malformed teamswitch event");
+      }
+      break;
+
+    case Constants.PLAYERS_SCORES:
+      console.log('PLAYER SCORES');
+
+      if (message.hasOwnProperty('red_players') && message.hasOwnProperty('blu_players')) {
+        message.red_players.forEach(function(red_player) {
+          if (red_player.hasOwnProperty('player') && red_player.hasOwnProperty('score')) {
+            _players[red_player.player].score = red_player.score;
+          }
+        });
+
+        message.blu_players.forEach(function(blu_player) {
+          if (blu_player.hasOwnPropert('player') && blu_player.hasOwnProperty('score')) {
+            _players[blu_player.player].score = blu_player.score;
+          }
+        });
+
+        TeamStore.emitChange();
+      } else {
+        console.log("Malformed playersscores event");
+      }
+
+      break;
+
+    case Constants.ROUND_OVER:
+      console.log('ROUND OVER');
+
+      if (message.hasOwnProperty('winning_team') && message.hasOwnProperty('red_score') && message.hasOwnProperty('blu_score')) {
+        _teamScores[_RED] = message.red_score;
+        _teamScores[_BLU] = message.blu_score;
+        TeamStore.emitChange();
+      } else {
+        console.log("Malformed roundover event");
+      }
+      break;
+
+    default:
+      console.log('Undefined action received.');
+      console.log(action);
+  }
 });
 
 module.exports = TeamStore;
